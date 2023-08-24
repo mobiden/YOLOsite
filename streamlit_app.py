a0c785133ea1f0b8c963ff4c756f65c66d533c1c
DEN <46221700+mobiden@users.noreply.github.com>
2023-08-24 11:40:39 +0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from ultralytics import YOLO
+import cv2
+import numpy as np
+
+#@st.cache_data
+
+model = YOLO("3506_best.pt")
+
+st.title('Поиск оружия на фотографиях')
+
+#picture = st.camera_input("Take a picture")
+
+#if picture:
+#    st.image(picture)
+
+uploaded_file = st.file_uploader(label="Загрузите кадр с оружием",
+                 type=['png', 'jpg', ],
+                 accept_multiple_files=False,
+                 key=None,
+                 help=None,
+                 on_change=None,
+                 args=None,
+                 disabled=False,
+                 label_visibility="visible")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    result = model.predict(img)
+    result = result[0].plot()
+    st.image(result)
